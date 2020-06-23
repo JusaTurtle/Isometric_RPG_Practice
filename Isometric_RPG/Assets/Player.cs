428d4a27acd7925e5c334baf8f6cecbea849d079
Hugh Phan <hughphandev@gmail.com>
2020-06-23 11:09:08 +0700

    Basic Movement

---
+++ Isometric_RPG/Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+    private float fallSpeed;
+    private CharacterController controller;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Vector2 inputDir = GetPlayerInput();
+        fallSpeed = controller.isGrounded ? 0 : fallSpeed + (gravity * Time.deltaTime);
+        controller.Move(GetVelocity(inputDir, fallSpeed) * Time.deltaTime);
+    }
+
+    private static Vector2 GetPlayerInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private Vector3 GetVelocity(Vector2 inputDir, float fallSpeed)
+    {
+        return CalculateOnGroundVelocity(inputDir) + CalculateFallVelocity(fallSpeed);
+    }
+
+    private Vector3 CalculateFallVelocity(float fallSpeed)
+    {
+        return fallSpeed * Vector3.down * speed;
+    }
+
+    private Vector3 CalculateOnGroundVelocity(Vector2 inputDir)
+    {
+        return ((inputDir.x * Vector3.right) + (inputDir.y * Vector3.forward)) * speed;
+    }
+}
---
+++ Isometric_RPG/Assets/ViewFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewFollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform view;
+    public Vector3 offSet;
+    public float smoothSpeed;
+
+    private void Update() {
+        view.position = Vector3.Lerp(view.position, target.position + offSet, smoothSpeed * Time.deltaTime);
+    }
+}
